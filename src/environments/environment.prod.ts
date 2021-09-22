68d0cb203cab4b9ab66e2a08dc26ee3a9ce823ff
joel <TeslaTeslon@fastweb.com>
2021-09-22 15:31:15 -0500

    deploy_

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base_url:'http://localhost:3000/api'
+  base_url:'https://tutorias003-back.herokuapp.com/api'
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+// http://localhost:3000/api before endpoint
 export const environment = {
   production: false,
-  base_url:'http://localhost:3000/api'
+  base_url:'https://tutorias003-back.herokuapp.com/api'
 };
 
 /*
